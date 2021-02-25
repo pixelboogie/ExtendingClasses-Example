a46697eba426fd96219e263f4aadd5279e1a07dc
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-02-24 16:07:22 -0800

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace MyGame{
+
+    public class Health
+    {
+        public int health = 1;
+        public int maxHealth = 5;
+
+        public float CurrentHealthPercent()
+        {
+            return (float)health/maxHealth * 100;
+        }
+
+        public virtual void RestoreHealth()
+        {
+            health = maxHealth;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyGame
+{
+    public class Player : Health
+    {
+        public Player()
+        {
+            RestoreHealth();
+        }
+
+        override public void RestoreHealth()
+        {
+            for(int i=health; i<= maxHealth; i++)
+            {
+                health = i;
+                Debug.Log ("Health " + health);
+            }
+        }
+
+
+    }
+}
